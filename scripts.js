6fd4f9d54fee395530f04d3a48f490e146cb13f4
mtkramer <kramerm1@mail.usf.edu>
2020-05-07 22:06:02 -0400

    debugging scripts

--- scripts.js
+++ scripts.js
@@ -1,43 +1,44 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let shuttle = {
-        height: 0,
-        changeHeight: (x) => {
-            shuttle.height += x;
-        },
-        down: 0,
-        right: 0
-    };
+let shuttle = {
+    height: 0,
+    changeHeight: (x) => {
+        shuttle.height += x;
+    },
+    down: 0,
+    right: 0
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    function id(x) { return document.getElementById(x); }
 
     id('takeoff').addEventListener('click', () => {
         if (confirm('Confirm that the shuttle is ready for takeoff.')) {
-            id('flightStatus').innerHTML = "Shuttle in flight.";
+            id('flightStatus').innerText = "Shuttle in flight.";
             id('shuttleBackground').style.backgroundColor = 'blue';
             shuttle.changeHeight(10000);
-            id('spaceShuttleHeight').innerHTML = String(shuttle.height);
+            id('spaceShuttleHeight').innerText = String(shuttle.height);
         }
         move();
     });
 
     id('landing').addEventListener('click', () => {
         alert('The shuttle is landing. Landing gear engaged.');
-        id('flightStatus').innerHTML = "The shuttle has landed.";
+        id('flightStatus').innerText = "The shuttle has landed.";
         id('shuttleBackground').style.backgroundColor = 'green';
         shuttle.changeHeight(-shuttle.height);
-        id('spaceShuttleHeight').innerHTML = String(shuttle.height);
+        id('spaceShuttleHeight').innerText = String(shuttle.height);
         move();
     });
 
     id('missionAbort').addEventListener('click', () => {
         if (confirm('Confirm that you want to abort the mission.')) {
-            id('flightStatus').innerHTML = "Mission aborted.";
+            id('flightStatus').innerText = "Mission aborted.";
             id('shuttleBackground').style.backgroundColor = 'green';
             shuttle.changeHeight(-shuttle.height);
-            id('spaceShuttleHeight').innerHTML = String(shuttle.height);
+            id('spaceShuttleHeight').innerText = String(shuttle.height);
         }
         move();
     });
-
-    function id(x) { return document.getElementById(x); }
 
     function move(direction) {
         if (direction === 'up') {
@@ -61,10 +62,3 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 });
-
-
-/*
-When the "Up", "Down", "Right", and "Left" buttons are clicked, the following should happen:
-The rocket image should move 10 px in the direction of the button that was clicked.
-If the "Up" or "Down" buttons were clicked, then the shuttle height should increase or decrease by 10,000 miles.
-*/
